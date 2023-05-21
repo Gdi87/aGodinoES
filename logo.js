819b35850f22e1f8e54f433e3dfc9c5ef261e675
AGB <129738915+Gdi87@users.noreply.github.com>
2023-05-21 21:31:27 +0200

    Add files via upload
    Signed-off-by: AGB <129738915+Gdi87@users.noreply.github.com>

---
+++ logo.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var logo = document.getElementById('logo');
+    
+    setTimeout(function() {
+      logo.classList.add('animate');
+    }, 100); // Retrasa la animación por 1 segundo (ajusta el valor según tus necesidades)
+  });
+  // Obtener el elemento del logo
